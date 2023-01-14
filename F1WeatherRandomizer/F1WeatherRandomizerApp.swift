0421e9f174232bb36d0d9418df3f35aa8952a8d7
Matthew Sacco <sacco1995@gmail.com>
2023-01-13 22:24:54 -0700

    Initial Commit

---
+++ F1WeatherRandomizer/F1WeatherRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1WeatherRandomizerApp.swift
+//  F1WeatherRandomizer
+//
+//  Created by Matthew Sacco on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct F1WeatherRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
